0b2a11e4667223ea73ba31279ff9b85202130ab8
Albin Rohde <albin.rohde@getflowbox.com>
2021-02-06 22:30:01 +0100

    Add socket io to server
    This makes it possible to run http and websocket concurrently on the
    same port, using http server to wrap both socketIo instance and the
    express server instance

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import "reflect-metadata"
-import express, {Application} from 'express'
+import http from 'http'
+import { Socket, Server } from 'socket.io'
+import sharedSession from 'express-socket.io-session'
+import express, {Application, Request} from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
@@ -10,16 +13,28 @@ import {User} from './user/models/User'
 
 import userRoute from './user/route'
 import gameRouter from "./game/route"
+import { Game } from "./game/models/Game"
 
-declare module "express-session" {
+declare module 'express-session' {
   interface Session {
     user: User
   }
 }
 
+// Set up app
 createConnection().then(async () => {
   console.log('connection to db established')
   const app: Application = express()
+  const server = http.createServer(app)
+  const io = new Server(server, {
+    cors: {
+      origin: "http://localhost",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["http://localhost", "user"],
+      credentials: true
+    }
+  })
+  
   app.use(bodyParser.urlencoded({extended: false}))
   app.use(bodyParser.json())
   app.use(cookieParser())
@@ -29,8 +44,7 @@ createConnection().then(async () => {
     res.header({'Access-Control-Allow-Headers': 'http://localhost'})
     next()
   })
-  
-  app.use(session({
+  const userSession = session({
     name: 'sid',
     secret: 'keyboard cat',
     resave: false,
@@ -41,11 +55,17 @@ createConnection().then(async () => {
       httpOnly: false,
       sameSite: false,
     }
-  }))
+  })
+  app.use(userSession)
+  io.use(sharedSession(userSession))
 
+  // Routes
   app.use('/user', userRoute)
   app.use('/game', gameRouter)
   
 
-  app.listen(5000, () => console.log(`Server started on port: 5000 `))
+  
+
+  // Start server
+  server.listen(5000, () => console.log(`Server started on port: 5000 `))
 }).catch(error => console.log(error));
