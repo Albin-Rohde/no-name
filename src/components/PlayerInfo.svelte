cedaeb1537acb95f484f521db6cee9cfa33efb0b
Albin Rohde <albin.rohde@getflowbox.com>
2021-02-07 02:35:40 +0100

    Add CopyText and playerinfo components

---
+++ src/components/PlayerInfo.svelte
@@ -0,0 +1,94 @@
+<script lang="typescript">
+  import type GameClientType from '../clients/GameClient'
+  export let gameClient: GameClientType
+</script>
+
+<div class="column">
+  <div class="players-header">
+    <p class="fs-4">Players</p>
+  </div>
+  <div class="score-header">
+    <p class="fs-4">Score</p>
+  </div>
+  <div class="divider">
+    <hr>
+  </div>
+  <div class="players">
+    <br>
+    {#each gameClient.users as user}
+      <p class="fs-5">{user.username}</p>
+    {/each}
+  </div>
+  <div class="score">
+    <br>
+    {#each gameClient.users as user}
+      <p class="fs-5">0</p>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .column {
+    display: grid;
+    grid-template-rows: 5% 2% auto;
+    grid-template-columns: 50% 50%;
+    max-width: 250px;
+    height: 60%;
+    margin-top: 20%;
+    grid-column-start: 1;
+    background-color: rgb(226, 226, 226);
+    box-shadow: 0px 2px 5px rgb(124, 124, 124);
+  }
+  .players-header {
+    grid-row-start: 1;
+    grid-column-start: 1;
+  }
+  
+  .score-header {
+    grid-row-start: 1;
+    grid-column-start: 2;
+  }
+
+  .divider {
+    grid-row-start: 2;
+    grid-column-start: 1;
+    grid-column-end: 3;
+  }
+
+  .players {
+    grid-row-start: 3;
+    grid-column-start: 1;
+    grid-column-end: 2;
+  }
+
+  .score {
+    grid-row-start: 3;
+    grid-column-start: 2;
+    grid-column-end: 3;
+    margin-right: 40%;
+    text-align: right;
+  }
+
+  .column p {
+    margin-left: 5%;
+  }
+  .players-header p{
+    margin-bottom: 0;
+  }
+  .players {
+    grid-column-start: 1;
+    grid-column-end: 2;
+  }
+</style>
+
+
+
+<!-- 
+<div class="players">
+    {#each gameClient.users as user}
+        <p class="fs-5">{user.username}</p>
+      {/each}
+  </div>
+
+
+-->
