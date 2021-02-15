67b2186f6888013187692331b89acbc1f984a4c5
albin <albin.rohde@notifyme.se>
2021-02-15 20:14:59 +0100

    Add card reference table
    This splits the card into 2 tables, one keeping track of the card data,
    and one keeping track of all the relations, who has which card, what
    game does the card belong to and so on

---
+++ src/card/models/WhiteCard.ts
@@ -0,0 +1,22 @@
+import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, JoinColumn, ManyToOne, Unique, OneToMany} from "typeorm"
+import { PlayerCard } from "./PlayerCard"
+
+@Entity('white_card')
+
+@Unique(['text'])
+export class WhiteCard extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({
+    default: null
+  })
+  deck: string
+
+  @Column({name: 'text'})
+  text: string
+
+	@OneToMany(type => PlayerCard, card => card.white_card)
+	@JoinColumn({name: 'player_cards'})
+	player_cards: PlayerCard[]
+}
