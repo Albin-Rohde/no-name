79a9fff5034a828f410377375052cbfc379f4929
albin <albin.rohde@notifyme.se>
2021-02-15 20:16:06 +0100

    Update card services to implement new card tables

--- src/card/services.ts
+++ src/card/services.ts
@@ -1,5 +1,6 @@
 import { getManager } from 'typeorm';
 import { PlayerCard } from './models/PlayerCard'
+import { WhiteCard } from './models/WhiteCard';
 
 const getCardById = async (id: number) => {
 	try {
@@ -10,24 +11,30 @@ const getCardById = async (id: number) => {
 }
 
 
-const getUniqueCard = async(count: number): Promise<PlayerCard> => {
+const getUniqueCard = async(count: number, game_key: string): Promise<PlayerCard> => {
 	const randomCardId = Math.floor(Math.random() * (count - 0))
-	const card = await PlayerCard.findOneOrFail(randomCardId, {relations: ['user']})
-	if(card && !card.user) {
-		return card
+	const whiteCard = await WhiteCard.findOneOrFail(randomCardId)
+	console.log(whiteCard)
+	if(await PlayerCard.findOne({white_card_id_fk: whiteCard.id, game_key: game_key})) {
+		// Card is already in current game.
+		return getUniqueCard(count, game_key)
 	}
-	return getUniqueCard(count)
+	const playerCard = new PlayerCard()
+	playerCard.white_card = whiteCard
+	playerCard.game_key = game_key
+	await playerCard.save()
+	return playerCard
 }
 
 
-const getUniqueCards = async (card_amount: number): Promise<PlayerCard[]> => {
+const getUniqueCards = async (card_amount: number, game_key: string): Promise<PlayerCard[]> => {
 	try {
 		const entityManager = getManager()
 		// remove type orm?
-		const [{count}] = await entityManager.query(`SELECT COUNT(*) FROM player_card`)
+		const [{count}] = await entityManager.query(`SELECT COUNT(*) FROM white_card`)
 		const cards: Promise<PlayerCard>[] = []
 		for(let i = 0; i < card_amount; i++) {
-			cards.push(getUniqueCard(count))
+			cards.push(getUniqueCard(count, game_key))
 		}
 		return Promise.all(cards)
 	} catch(err) {
