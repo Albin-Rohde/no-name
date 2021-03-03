44e3f5630af8da13508fa066b90713650a99935e
albin <albin.rohde@notifyme.se>
2021-03-03 21:52:54 +0100

    Save user instance to db in makeGameResponse
    Since this function will be the last one thats called, this is where we
    want to save our manipulated user instance to the db

--- src/game/normalizeRespose.ts
+++ src/game/normalizeRespose.ts
@@ -66,7 +66,8 @@ const normalizeCardResponse = (card: PlayerCard): CardResponse => {
 	}
 }
 
-export const makeGameResponse = async (game: Game): Promise<GameResponse> => {
-	const currentRound = await GameRound.findOne({game_key: game.key, round_number: game.current_round})
-	return normalizeGameResponse(game, currentRound)
+export const makeGameResponse = async (user: User): Promise<GameResponse> => {
+	await user.save()
+	const currentRound = await GameRound.findOne({game_key: user.game.key, round_number: user.game.current_round})
+	return normalizeGameResponse(user.game, currentRound)
 }
