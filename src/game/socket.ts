3dafa7871db0bf870f23e35c433642dc9a196d2f
albin <albin.rohde@notifyme.se>
2021-03-03 21:47:56 +0100

    Use the user instance for all modifications of game
    Also let errors bubble up to the global error handler

--- src/game/socket.ts
+++ src/game/socket.ts
@@ -1,41 +1,24 @@
 import type { Server, Socket } from 'socket.io'
 import { makeGameResponse } from './normalizeRespose'
-import { Game } from "./models/Game"
 import { addPlayerToGame, handlePlayCard, startGame } from './services'
 
 const joinGameEvent = async (io: Server, socket: Socket, key: string) => {
-	try {
-		const user = socket.request.session.user
-		const game = await Game.findOneOrFail(key, {relations: ['users', 'users.cards', 'users.cards.white_card']})
-		await addPlayerToGame(game, user)
-		socket.join(key)
-		io.in(game.key).emit('update', await makeGameResponse(game))
-	} catch(err) {
-		socket.emit('connection_error', err.message)
-	}
+	const {user} = socket.request.session
+	await addPlayerToGame(user, key)
+	socket.join(key)
+	io.in(user.game.key).emit('update', await makeGameResponse(user))
 }
 
 const startGameEvent = async (io: Server, socket: any) => {
-	try {
-		const user = socket.request.session.user
-		const game = await Game.findOneOrFail(user.game.key, {relations: ['users', 'users.cards', 'users.cards.white_card']})
-		await startGame(game)
-		io.in(game.key).emit('update', await makeGameResponse(game))
-	} catch(err) {
-		console.log(err)
-		socket.emit('connection_error', err.message)
-	}
+	const {user} = socket.request.session
+	await startGame(user.game)
+	io.in(user.game.key).emit('update', await makeGameResponse(user))
 }
 
 const playCardEvent = async(io: Server, socket: any, cardId: number) => {
-	try {
-		const user = socket.request.session.user
-		await handlePlayCard(user, cardId)
-		const game = await Game.findOneOrFail(user.game.key, {relations: ['users', 'users.cards', 'users.cards.white_card']})
-		io.in(game.key).emit('update', await makeGameResponse(game))
-	} catch(err) {
-		socket.emit('connection_error', err.message)
-	}
+	const {user} = socket.request.session
+	await handlePlayCard(user, cardId)
+	io.in(user.game.key).emit('update', await makeGameResponse(user))
 }
 
 export {joinGameEvent, startGameEvent, playCardEvent}
