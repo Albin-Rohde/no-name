1a9942bd2e437cf77f4ab379dd2743c041e587cd
Albin Rohde <albin.rohde@advinans.se>
2021-03-25 13:26:07 +0100

    Retrieve user as db instance on every event
    This ensures that the user is up to date with the database data about a user, Also makes it possible to do user.save() etc.
    
    This solve a bug with joining a game.

--- server/src/socket/events.ts
+++ server/src/socket/events.ts
@@ -2,6 +2,7 @@ import type { Server, Socket } from 'socket.io'
 import { authSocketUser } from '../authenticate'
 import { makeGameResponse } from '../game/normalizeRespose'
 import { addPlayerToGame, handlePlayCard, startGame } from './services'
+import { getUserWithRelation } from '../user/services'
 
 const socketEventHandler = async (socket: Socket, io: Server, next: any) => {
 	try {
@@ -17,20 +18,20 @@ const socketEventHandler = async (socket: Socket, io: Server, next: any) => {
 }
 
 const joinGameEvent = async (io: Server, socket: Socket, key: string) => {
-	const {user} = socket.request.session
+	const user = await getUserWithRelation(socket.request.session.user.id)
 	await addPlayerToGame(user, key)
 	socket.join(key)
 	io.in(user.game.key).emit('update', await makeGameResponse(user))
 }
 
 const startGameEvent = async (io: Server, socket: Socket) => {
-	const {user} = socket.request.session
+	const user = await getUserWithRelation(socket.request.session.user.id)
 	await startGame(user.game)
 	io.in(user.game.key).emit('update', await makeGameResponse(user))
 }
 
 const playCardEvent = async(io: Server, socket: Socket, cardId: number) => {
-	const {user} = socket.request.session
+	const user = await getUserWithRelation(socket.request.session.user.id)
 	await handlePlayCard(user, cardId)
 	io.in(user.game.key).emit('update', await makeGameResponse(user))
 }
