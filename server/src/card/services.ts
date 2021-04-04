60ac16454cc1221f8c11ff6df41590cafe72b782
Albin Rohde <albin.rohde@advinans.se>
2021-04-05 01:14:19 +0200

    Refactor and bugfix of get uniqueCards
    There was a lot of issues in this code creating a lot of strange bugs, this removes complexity, and is imo a easier flow to follow.
    
    This also solves the bug where the user would not be asigned correct amoutn of cards. This due to some unexpected behaviout of how the new cards where saved to db.

--- server/src/card/services.ts
+++ server/src/card/services.ts
@@ -1,39 +1,41 @@
-import { getManager } from 'typeorm';
-import { PlayerCard } from './models/PlayerCard'
-import { WhiteCard } from './models/WhiteCard';
-import type { User } from '../user/models/User';
+import {getManager} from 'typeorm'
+import {CardState, PlayerCard} from './models/PlayerCard'
+import { WhiteCard } from './models/WhiteCard'
+import type {User} from '../user/models/User'
 
-
-const getUniqueWhiteCard = async(count: number, gameKey: string) => {
-	const randomCardId = Math.floor(Math.random() * (count - 0)) + 1
-	let whiteCard = await WhiteCard.findOneOrFail(randomCardId)
-	if(await PlayerCard.findOne({white_card_id_fk: whiteCard.id, game_key: gameKey})) {
-		whiteCard = await getUniqueWhiteCard(count, gameKey)
-		return whiteCard
+const getRandomCardId = (count: number, omitIds: number[] = []): number => {
+	const randomCardId = Math.floor(Math.random() * (count)) + 1
+	if(omitIds.some((id) => id === randomCardId)) {
+		return getRandomCardId(count, omitIds)
 	}
-	return whiteCard
+	return randomCardId
 }
-
-const getUniquePlayCard = async(count: number, game_key: string, user: User): Promise<PlayerCard> => {
-	const whiteCard = await getUniqueWhiteCard(count, game_key)
-	const playerCard = new PlayerCard()
-	playerCard.white_card = whiteCard
-	playerCard.game_key = game_key
-	playerCard.user = user
-	playerCard.user_id_fk = user.id
-	return playerCard.save()
-}
-
 
 const getUniqueCards = async (card_amount: number, game_key: string, user: User): Promise<PlayerCard[]> => {
 	try {
 		const entityManager = getManager()
 		const [{count}] = await entityManager.query(`SELECT COUNT(*) FROM white_card`)
-		const cards: Promise<PlayerCard>[] = []
+		const usedCards: number[] = await entityManager.query(`
+			SELECT wc.id FROM white_card AS wc
+			JOIN player_card_ref AS pcr ON wc.id = pcr.white_card_id_fk
+			WHERE pcr.game_key = '${game_key}';
+		`)
+
+		const cards: PlayerCard[] = []
 		for(let i = 0; i < card_amount; i++) {
-			cards.push(getUniquePlayCard(count, game_key, user))
+			const whiteCard = await WhiteCard.findOneOrFail(getRandomCardId(count, usedCards))
+			usedCards.push(whiteCard.id)
+			const card = new PlayerCard()
+			card.user = user
+			card.user_id_fk = user.id
+			card.game_key = game_key
+			card.state = CardState.HAND
+			card.white_card = whiteCard
+			card.white_card_id_fk = whiteCard.id
+			cards.push(card)
 		}
-		return Promise.all(cards)
+
+		return Promise.all(cards.map(c => c.save()))
 	} catch(err) {
 		console.log(err)
 		throw new Error('Error assigning white cards to user')
