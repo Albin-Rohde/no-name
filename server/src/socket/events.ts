51388961124318dd7976511094b0c9eae69243eb
Albin Rohde <albin.rohde@advinans.se>
2021-04-08 00:14:51 +0200

    Use Game as root Model instead of the User model

--- server/src/socket/events.ts
+++ server/src/socket/events.ts
@@ -2,6 +2,7 @@ import type { Server, Socket } from 'socket.io'
 import { makeGameResponse } from '../game/normalizeRespose'
 import { addPlayerToGame, handlePlayCard, startGame } from './services'
 import { getUserWithRelation } from '../user/services'
+import { getGameFromUser, getGameWithRelations } from "../game/services";
 
 const socketEventHandler = async (socket: Socket, io: Server, next: any) => {
   socket.on('join', (key: string) => joinGameEvent(io, socket, key))
@@ -13,8 +14,9 @@ const socketEventHandler = async (socket: Socket, io: Server, next: any) => {
 
 const joinGameEvent = async (io: Server, socket: Socket, key: string) => {
   try {
+    const game = await getGameWithRelations(key)
     const user = await getUserWithRelation(socket.request.session.user.id)
-    const game = await addPlayerToGame(user, key)
+    game.addPlayer(user)
     socket.join(key)
     io.in(key).emit('update', await makeGameResponse(game))
   } catch(err) {
@@ -25,9 +27,14 @@ const joinGameEvent = async (io: Server, socket: Socket, key: string) => {
 
 const startGameEvent = async (io: Server, socket: Socket) => {
   try {
-    const user = await getUserWithRelation(socket.request.session.user.id)
-    await startGame(user)
-    io.in(user.game.key).emit('update', await makeGameResponse(user.game))
+    const game = await getGameFromUser(socket.request.session.user.id)
+    if(game.started) throw new Error('Game already started')
+    game.started = true
+    await Promise.all([
+      game.handOutCards(),
+      game.createRounds(),
+    ])
+    io.in(game.key).emit('update', await makeGameResponse(game))
   } catch(err) {
     console.error(err)
     socket.emit('connection_error', err.message)
@@ -36,9 +43,9 @@ const startGameEvent = async (io: Server, socket: Socket) => {
 
 const playCardEvent = async(io: Server, socket: Socket, cardId: number) => {
   try {
-    const user = await getUserWithRelation(socket.request.session.user.id)
-    await handlePlayCard(user, cardId)
-    io.in(user.game.key).emit('update', await makeGameResponse(user.game))
+    const game = await getGameFromUser(socket.request.session.user.id)
+    await game.currentUser.playCard(cardId)
+    io.in(game.key).emit('update', await makeGameResponse(game))
   } catch(err) {
     console.error(err)
     socket.emit('connection_error', err.message)
@@ -47,9 +54,9 @@ const playCardEvent = async(io: Server, socket: Socket, cardId: number) => {
 
 const getGameEvent = async(io: Server, socket: Socket) => {
   try {
-    const user = await getUserWithRelation(socket.request.session.user.id)
-    if (user.game) {
-      socket.emit('update', await makeGameResponse(user.game))
+    const game = await getGameFromUser(socket.request.session.user.id)
+    if (game) {
+      socket.emit('update', await makeGameResponse(game))
     }
   } catch(err) {
     console.error(err)
