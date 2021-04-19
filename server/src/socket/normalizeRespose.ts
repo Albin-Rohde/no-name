eed4bc6f063107318420ad189bd5550dfeeca4d4
Albin Rohde <albin.rohde@advinans.se>
2021-04-19 21:02:17 +0200

    refactor normalize functions

--- server/src/socket/normalizeRespose.ts
+++ server/src/socket/normalizeRespose.ts
@@ -34,37 +34,31 @@ interface CardResponse {
   state: CardState
 }
 
-export const normalizeGameResponse = (game: Game, currentRound: GameRound | undefined): GameResponse => {
-  return {
-    key: game.key,
-    gameOptions: {
-      deck: game.card_deck,
-      cardLimit: game.play_cards,
-      playerLimit: game.player_limit,
-      privateLobby: game.private_lobby,
-      rounds: game.rounds
-    },
-    started: game.started,
-    users: game.users ? [...game.users.map(user => normalizeUserResponse(user, currentRound))] : []
-  }
-}
+export const normalizeGameResponse = (game: Game, currentRound: GameRound | undefined): GameResponse => ({
+  key: game.key,
+  gameOptions: {
+    deck: game.card_deck,
+    cardLimit: game.play_cards,
+    playerLimit: game.player_limit,
+    privateLobby: game.private_lobby,
+    rounds: game.rounds
+  },
+  started: game.started,
+  users: game.users ? [...game.users.map(user => normalizeUserResponse(user, currentRound))] : []
+})
 
-const normalizeUserResponse = (user: User, currentRound: GameRound | undefined): UserResponse => {
-  return {
-    id: user.id,
-    username: user.username,
-    cards: user.cards ? [...user.cards.map(normalizeCardResponse)] : [],
-    cardWizz: currentRound?.card_wizz_user_id_fk === user.id,
-    hasPlayed: user.hasPlayed,
-    isHost: user.isHost,
-    score: user.score,
-  }
-}
+const normalizeUserResponse = (user: User, currentRound: GameRound | undefined): UserResponse => ({
+  id: user.id,
+  username: user.username,
+  cards: user.cards ? [...user.cards.map(normalizeCardResponse)] : [],
+  cardWizz: currentRound?.card_wizz_user_id_fk === user.id,
+  hasPlayed: user.hasPlayed,
+  isHost: user.isHost,
+  score: user.score,
+})
 
-const normalizeCardResponse = (card: PlayerCard): CardResponse => {
-  return {
-    id: card.id,
-    text: card.white_card.text,
-    state: card.state
-  }
-}
+const normalizeCardResponse = (card: PlayerCard): CardResponse => ({
+  id: card.id,
+  text: card.white_card.text,
+  state: card.state,
+})
