174f2d884ec5d9805c3d9821b476ce9612a4427d
Albin Rohde <albin.rohde@notifyme.se>
2021-05-14 15:35:58 +0200

    fix issue in queries

--- server/src/rest/game/services.ts
+++ server/src/rest/game/services.ts
@@ -22,33 +22,29 @@ interface GameSettings {
  * @param options GameSettings object, containing settings for the game
  */
 export async function createNewGame (user: User, options: GameSettings): Promise<Game> {
-  try {
-    if(user.game) {
-      await deleteGame(user)
-    }
-    const game = new Game()
-    game.key = uuidv4()
-    game.playCards = options.playCards
-    game.playerLimit = options.playerLimit
-    game.privateLobby = options.private
-    game.rounds = options.rounds
-    game.cardDeck = 'default'
-    game.hostUserId = user.id
-    user.game = game
-
-    let gameRounds = []
-    for(let i = 1; i <= game.rounds; i++) {
-      const gameRound = new GameRound()
-      gameRound.game_key = game.key
-      gameRound.round_number = i
-      gameRounds.push(gameRound.save())
-    }
-    await Promise.all(gameRounds)
-    await user.save()
-    return game
-  } catch(err) {
-    throw new Error(err)
+  if(user.game) {
+    await deleteGame(user)
   }
+  const game = new Game()
+  game.key = uuidv4()
+  game.playCards = options.playCards
+  game.playerLimit = options.playerLimit
+  game.privateLobby = options.private
+  game.rounds = options.rounds
+  game.cardDeck = 'default'
+  game.hostUserId = user.id
+  user.game = game
+
+  let gameRounds = []
+  for(let i = 1; i <= game.rounds; i++) {
+    const gameRound = new GameRound()
+    gameRound.game_key = game.key
+    gameRound.round_number = i
+    gameRounds.push(gameRound.save())
+  }
+  await Promise.all(gameRounds)
+  await user.save()
+  return game
 }
 
 
@@ -66,24 +62,24 @@ export async function deleteGame (user: User): Promise<void> {
     throw new GameRuleError('User is not host, Only host can delete game')
   }
   const gameKey = user.game_fk
-  const updateUsers = User.createQueryBuilder('user')
-    .where('user.game_fk = :gameKey', {gameKey})
+  const updateUsers = User.createQueryBuilder()
+    .where('game_fk = :gameKey', {gameKey})
     .update({
       game_fk: null,
       score: 0,
       hasPlayed: false,
     })
     .execute()
-  const deleteWcr = WhiteCardRef.createQueryBuilder('wcr')
-    .where('wcr.game_key =:gameKey', {gameKey})
+  const deleteWcr = WhiteCardRef.createQueryBuilder()
+    .where('game_key =:gameKey', {gameKey})
     .delete()
     .execute()
-  const deleteGame = Game.createQueryBuilder('game')
-    .where('game.id = :gameKey', {gameKey})
+  const deleteGame = Game.createQueryBuilder()
+    .where('key = :gameKey', {gameKey})
     .delete()
     .execute()
-  const deleteGameRound = GameRound.createQueryBuilder('gr')
-    .where('gr.game_key_fk = :gameKey', {gameKey})
+  const deleteGameRound = GameRound.createQueryBuilder()
+    .where('game_key_fk = :gameKey', {gameKey})
     .delete()
     .execute()
   await Promise.all([updateUsers, deleteWcr, deleteGame, deleteGameRound])
