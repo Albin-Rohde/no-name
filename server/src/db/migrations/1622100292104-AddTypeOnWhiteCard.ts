a1b8f1890a30ad598dbae791ab23adeb4387f25c
Albin Rohde <albin.rohde@notifyme.se>
2021-05-29 18:00:31 +0200

    add remaining swedish cards
    also fix some issues with unique constraints

--- server/src/db/migrations/1622100292104-AddTypeOnWhiteCard.ts
+++ server/src/db/migrations/1622100292104-AddTypeOnWhiteCard.ts
@@ -9,6 +9,8 @@ export class AddTypeOnWhiteCard1622100292104 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "white_card" ADD "type" "white_card_type_enum" NOT NULL DEFAULT 'unknown'`);
         await queryRunner.query(`ALTER TABLE "white_card" DROP CONSTRAINT "UQ_9c4c340371e71ab2de609423bfc"`);
         await queryRunner.query(`ALTER TABLE "white_card" ADD CONSTRAINT "UQ_0723849a4fcf5cae3d13fc6bad8" UNIQUE ("deck", "text")`);
+        await queryRunner.query(`ALTER TABLE "black_card" DROP CONSTRAINT "UQ_e072db27ec070353974390f8b39"`);
+        await queryRunner.query(`ALTER TABLE "black_card" ADD CONSTRAINT "UQ_e072db27ec070353974390f8b39" UNIQUE ("deck", "text")`);
 
         const sql_white_verb = await addCardsToDb('white_verb')
         const sql_white_noun = await addCardsToDb('white_noun')
@@ -27,6 +29,7 @@ export class AddTypeOnWhiteCard1622100292104 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "white_card" ADD CONSTRAINT "UQ_9c4c340371e71ab2de609423bfc" UNIQUE ("text")`);
         await queryRunner.query(`ALTER TABLE "white_card" DROP COLUMN "type"`);
         await queryRunner.query(`DROP TYPE "white_card_type_enum"`);
+        await queryRunner.query(`ALTER TABLE "black_card" DROP CONSTRAINT "UQ_e072db27ec070353974390f8b39"`);
     }
 
 }
