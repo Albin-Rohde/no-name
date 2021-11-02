e87aae429f922aa0772097d4bb8df40358bceefc
Albin Rohde <albin.rohde@advinans.se>
2021-11-02 12:14:12 +0100

    fix error regarding deploy docker and env

--- mobile/src/clients/SocketClient.ts
+++ mobile/src/clients/SocketClient.ts
@@ -24,7 +24,7 @@ enum Events {
 }
 
 export class SocketClient {
-  private readonly baseUrl: string = process.env.REACT_APP_API_BASE_URL;
+  private readonly baseUrl: string = process.env.REACT_APP_SOCKET_BASE_URL;
   private game: Game
   public socket: Socket
   public nextGameKey: string
