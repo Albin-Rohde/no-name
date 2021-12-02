b8e00ce00560c01468702037bb78b553355146df
Albin Rohde <albin.rohde@advinans.se>
2021-12-02 15:24:02 +0100

    refactor rendering of cards

--- frontend/src/screens/loggedIn/inGame/Game.tsx
+++ frontend/src/screens/loggedIn/inGame/Game.tsx
@@ -4,19 +4,18 @@ import {Box, Card, CardContent, Grid, Paper, Typography} from "@mui/material";
 import PlayerDrawer from "../../../components/PlayerDrawer";
 import DataTable from "../../../components/DataTable";
 import ConfirmPlayModal from "../../../components/ConfirmPlayModal";
-import {CardResponse, CardState} from "../../../clients/ResponseTypes";
+import {CardResponse} from "../../../clients/ResponseTypes";
 import {SocketClient} from "../../../clients/SocketClient";
 import {useSelector} from "react-redux";
 import {ReduxState} from "../../../redux/redux";
 import * as GameClient from '../../../clients/Game';
-import {GameState} from "../../../clients/Game";
+import RenderCards from "./components/RenderCards";
 
 const Game = () => {
   const [confirmPlayOpen, setConfirmPlayOpen] = useState(false);
   const [openCard, setOpenCard] = useState<CardResponse>(null);
   const game = useSelector<ReduxState, GameClient.default>((state) => state.game);
   const socket = useSelector<ReduxState, SocketClient>((state) => state.socket);
-  let cardsRefMap = new Map();
 
   const handleCardClick = (card: CardResponse) => {
     if (!game.currentUser.hasPlayed) {
@@ -24,284 +23,24 @@ const Game = () => {
       setConfirmPlayOpen(true);
     }
   }
-
   const handleFlipClick = (card: CardResponse) => {
     socket.flipCard(card);
   }
-
   const handleVoteClick = (card: CardResponse) => {
     socket.voteCard(card);
   }
-
-  const closeModal = () => {
-    setConfirmPlayOpen(false);
-    setOpenCard(null);
-  }
-
   const handlePlayCard = () => {
     socket.playCard(openCard);
     setOpenCard(null);
     setConfirmPlayOpen(false);
   }
-
+  const closeModal = () => {
+    setConfirmPlayOpen(false);
+    setOpenCard(null);
+  }
   const isMobile = window.screen.width < 800;
 
-  const renderPlaying = () => {
-    if (isMobile) {
-      return (
-        <Box>
-          <Typography color={'white'} variant={'body1'} align={'center'}>
-            Your cards
-          </Typography>
-          <Typography color={'white'} variant={'body1'} align={'center'}>
-            Played Cards ({game.playedCards.length}/{game.players.length})
-          </Typography>
-          <Box sx={{
-            display: 'flex',
-            overflow: 'scroll',
-            marginTop: '2vh',
-            scrollSnapType: 'x mandatory',
-          }}>
-            {renderCards(game.cardsOnHand, handleCardClick)}
-          </Box>
-        </Box>
-      )
-    } else {
-      return (
-        <Box>
-          <Typography color={'white'} variant={'body1'} align={'center'}>
-            Your cards
-          </Typography>
-          <Typography color={'white'} variant={'body1'} align={'center'}>
-            Played Cards ({game.playedCards.length}/{game.players.length})
-          </Typography>
-          <Box sx={{display: 'flex', justifyContent: 'center', marginTop: '2vh'}}>
-            {renderCards(game.cardsOnHand, handleCardClick)}
-          </Box>
-        </Box>
-      )
-    }
-  }
-
-  const renderFlipping = () => {
-    const cardWizzHelptext = () => {
-      if (game.playedCards.length < game.players.length) {
-        return 'Wait for everyone to play';
-      } else {
-        return 'Click the cards to flip them'
-      }
-    }
-    const playerHelpText = () => {
-      if (game.playedCards.length < game.players.length) {
-        return 'Wait for everyone to play';
-      } else {
-        return 'Wait for card wizz to flip all cards';
-      }
-    }
-    const renderHelpText = () => {
-      if (game.currentUser.cardWizz) {
-        return (
-          <Typography color={'white'} variant={'body1'} align={'center'}>
-            {cardWizzHelptext()}
-          </Typography>
-        )
-      }
-      return (
-        <Typography color={'white'} variant={'body1'} align={'center'}>
-          {playerHelpText()}
-        </Typography>
-      )
-    }
-    if (isMobile) {
-      return (
-        <Box>
-          {renderHelpText()}
-          <Typography color={'white'} variant={'body1'} align={'center'}>
-            Flipped cards ({game.playedCards.filter(c => c.state === CardState.PLAYED_SHOW).length}/{game.players.length})
-          </Typography>
-          <Box sx={{
-            display: 'flex',
-            overflow: 'scroll',
-            marginTop: '2vh',
-            scrollSnapType: 'x mandatory',
-          }}>
-            {renderCards(game.playedCards, handleFlipClick)}
-          </Box>
-        </Box>
-      )
-    } else {
-      return (
-        <Box>
-          {renderHelpText()}
-          <Typography color={'white'} variant={'body1'} align={'center'}>
-            Flipped cards ({game.playedCards.filter(c => c.state === CardState.PLAYED_SHOW).length}/{game.players.length})
-          </Typography>
-          <Box sx={{display: 'flex', justifyContent: 'center', marginTop: '2vh'}}>
-            {renderCards(game.playedCards, handleFlipClick)}
-          </Box>
-        </Box>
-      )
-    }
-  }
-
-  const renderVoting = () => {
-    const cardWizzHelptext = () => {
-      return 'Click on the best card'
-    }
-    const playerHelpText = () => {
-      return 'Wait for card wizz to select winner';
-    }
-    const renderHelpText = () => {
-      if (game.currentUser.cardWizz) {
-        return (
-          <Typography color={'white'} variant={'body1'} align={'center'}>
-            {cardWizzHelptext()}
-          </Typography>
-        )
-      }
-      return (
-        <Typography color={'white'} variant={'body1'} align={'center'}>
-          {playerHelpText()}
-        </Typography>
-      )
-    }
-    if (isMobile) {
-      return (
-        <Box>
-          {renderHelpText()}
-          <Box sx={{
-            display: 'flex',
-            overflow: 'scroll',
-            marginTop: '2vh',
-            scrollSnapType: 'x mandatory',
-          }}>
-            {renderCards(game.playedCards, handleVoteClick)}
-          </Box>
-        </Box>
-      )
-    } else {
-      return (
-        <Box>
-          {renderHelpText()}
-          <Box sx={{display: 'flex', justifyContent: 'center', marginTop: '2vh'}}>
-            {renderCards(game.playedCards, handleVoteClick)}
-          </Box>
-        </Box>
-      )
-    }
-  }
-
-  const renderWin = () => {
-    if (isMobile) {
-      return (
-        <Box>
-          <Typography color={'white'} variant={'body1'} align={'center'}>
-            {game.winningPlayer.username} won this round with the card:
-          </Typography>
-          <Box sx={{
-            display: 'flex',
-            overflow: 'scroll',
-            marginTop: '2vh',
-            scrollSnapType: 'x mandatory',
-          }}>
-            {renderCards([game.winnerCard], () => null)}
-          </Box>
-        </Box>
-      )
-    } else {
-      return (
-        <Box>
-          <Typography color={'white'} variant={'body1'} align={'center'}>
-            {game.winningPlayer.username} won this round with the card:
-          </Typography>
-          <Box sx={{display: 'flex', justifyContent: 'center', marginTop: '2vh'}}>
-            {renderCards([game.winnerCard], () => null)}
-          </Box>
-        </Box>
-      )
-    }
-  }
-
-  const getCardText = (card: CardResponse) => {
-    const mergedCardText = () => {
-      const blackTextParts = game.blackCard.text.split('_');
-      return (<React.Fragment>{blackTextParts[0]}<b>{card.text}</b> {blackTextParts[1]}</React.Fragment>);
-    }
-    switch (card.state) {
-      case CardState.HAND:
-        return card.text;
-      case CardState.PLAYED_SHOW:
-        return mergedCardText();
-      case CardState.WINNER:
-        return mergedCardText();
-      case CardState.PLAYED_HIDDEN:
-        return '';
-    }
-  }
-
-  type CardClickCb = (card: CardResponse) => void;
-  const renderCards = (cards: CardResponse[], clickCb: CardClickCb) => {
-    return cards.map((card) => {
-      return (
-        <Card
-          sx={{
-            marginRight: isMobile ? '2%' : '5px',
-            marginLeft: isMobile ? '2%' : '5px',
-            minHeight: '250px',
-            maxHeight: '250px',
-            minWidth: isMobile ? '200px' : '11.8%',
-            maxWidth: isMobile ? '200px' : '11.8%',
-            backgroundColor: '#505050',
-            scrollSnapAlign: 'center',
-            cursor: 'pointer',
-          }}
-          ref={cardsRefMap.get(card.id)}
-          className='noSelect' // prevent some default behaviour regarding clickables.
-          onClick={() => clickCb(card)}
-        >
-          <CardContent>
-            <Typography variant={'h5'}>
-              {getCardText(card)}
-            </Typography>
-          </CardContent>
-        </Card>
-      )
-    })
-  }
-
-  const renderCardsView = () => {
-    if (game.currentUser.cardWizz) {
-      switch (game.state) {
-        case GameState.PLAYING:
-          return renderFlipping();
-        case GameState.FLIPPING:
-          return renderFlipping();
-        case GameState.VOTING:
-          return renderVoting();
-        case GameState.DISPLAY_WINNER:
-          return renderWin();
-        default:
-          return renderFlipping();
-      }
-    }
-    switch (game.state) {
-      case GameState.PLAYING:
-        if (game.currentUser.hasPlayed) {
-          return renderFlipping();
-        }
-        return renderPlaying();
-      case GameState.FLIPPING:
-        return renderFlipping();
-      case GameState.VOTING:
-        return renderVoting();
-      case GameState.DISPLAY_WINNER:
-        return renderWin();
-      default:
-        return renderPlaying();
-    }
-  }
-
-  const mainGridHeight = isMobile ? 'calc(94vh - 430px)' : 'calc(94vh - 400px)'
+  const mainGridHeight = isMobile ? 'calc(94vh - 430px)' : 'calc(94vh - 21vw)'
   return (
     <React.Fragment>
       <Grid container sx={{width: '100%', marginTop: '6vh', height: mainGridHeight}}>
@@ -315,8 +54,8 @@ const Game = () => {
                 backgroundColor: '#282c36',
                 marginBottom: '3vh'
               }}>
-                <Typography color='white' variant={'body1'} sx={{marginLeft: '3vw'}}>
-                  Current Card wizz: {game.users.find(u => u.cardWizz).username}
+                <Typography color='white' variant={'body1'} sx={{marginLeft: '2vw'}}>
+                  Round {game.currentRound} of {game.gameOptions.rounds}
                 </Typography>
               </Paper>
             </Box>
@@ -325,10 +64,10 @@ const Game = () => {
         <Grid item xs={8} sm={6} md={6} sx={{display: 'flex', justifyContent: 'center'}}>
           <Card
             sx={{
-              minHeight: '250px',
-              maxHeight: '260px',
-              minWidth: '200px',
-              maxWidth: '200px',
+              minHeight: isMobile ? '250px' : '12.0vw',
+              maxHeight: isMobile ? '260px' : '12.0vw',
+              minWidth: isMobile ? '200px' : '10.8vw',
+              maxWidth: isMobile ? '200px' : '10.8vw',
             }}
           >
             <CardContent>
@@ -343,8 +82,17 @@ const Game = () => {
       <Box sx={{
         display: 'flex',
         flexDirection: 'column',
+        marginTop: '3vh',
       }}>
-        {renderCardsView()}
+        <RenderCards
+          {...{
+            game,
+            handleCardClick,
+            handleFlipClick,
+            handleVoteClick,
+            handlePlayCard
+          }}
+        />
       </Box>
       <Grid item sm={3} md={4}/>
       <ConfirmPlayModal
---
+++ frontend/src/screens/loggedIn/inGame/components/RenderCards.tsx
@@ -0,0 +1,117 @@
+import Game, {GameState} from "../../../../clients/Game";
+import {BlackCardResponse, CardResponse} from "../../../../clients/ResponseTypes";
+import React from "react";
+import {Box, Typography} from "@mui/material";
+import CardsList from "./CardsList";
+
+type CardClickCallback = (card: CardResponse) => void;
+
+interface RenderCardsProps {
+  game: Game;
+  handleCardClick: CardClickCallback,
+  handleFlipClick: CardClickCallback,
+  handleVoteClick: CardClickCallback,
+}
+
+interface CardsProps {
+  cards: CardResponse[];
+  blackCard: BlackCardResponse;
+  cardClickCb: (card: CardResponse) => any;
+  helpText: string;
+}
+
+const RenderCards = (props: RenderCardsProps) => {
+  function getCardsState(): CardsProps {
+    if (props.game.state === GameState.DISPLAY_WINNER) {
+      return {
+        cards: [props.game.winnerCard],
+        cardClickCb: (_c) => null,
+        helpText: `${props.game.winningPlayer.username} bet all of you with their magic combo`,
+        blackCard: props.game.blackCard,
+      }
+    }
+    const playedStatus = `${props.game.playedCards.length}/${props.game.players.length}`
+    const cardProps: Partial<CardsProps> = { blackCard: props.game.blackCard };
+    if (props.game.currentUser.cardWizz) {
+      cardProps.cards = props.game.playedCards;
+      switch (props.game.state) {
+        case GameState.PLAYING:
+          cardProps.helpText = `You are the cardwizz. Wait for everyone to play ${playedStatus}`;
+          cardProps.cardClickCb = props.handleFlipClick;
+          break;
+        case GameState.FLIPPING:
+          cardProps.helpText = `You are the cardwizz. Flip the cards`;
+          cardProps.cardClickCb = props.handleFlipClick;
+          break;
+        case GameState.VOTING:
+          cardProps.helpText = `You are the cardwizz. Pick a winner!`;
+          cardProps.cardClickCb = props.handleVoteClick;
+          break;
+        default:
+          cardProps.helpText = `You are the cardwizz. Wait for everyone to play ${playedStatus}`;
+          cardProps.cardClickCb = props.handleFlipClick;
+      }
+    } else {
+      switch (props.game.state) {
+        case GameState.PLAYING:
+          if (props.game.currentUser.hasPlayed) {
+            cardProps.helpText = `Wait for everyone to play ${playedStatus}`;
+            cardProps.cards = props.game.playedCards;
+            cardProps.cardClickCb = props.handleFlipClick;
+            break;
+          }
+          cardProps.helpText = `Pick a funny card`;
+          cardProps.cards = props.game.cardsOnHand;
+          cardProps.cardClickCb = props.handleCardClick;
+          break;
+        case GameState.FLIPPING:
+          cardProps.helpText = `Wait for the cardwizz to flip all cards`;
+          cardProps.cards = props.game.playedCards;
+          cardProps.cardClickCb = props.handleFlipClick;
+          break;
+        case GameState.VOTING:
+          cardProps.helpText = `Wait for the cardwizz to pick winner`;
+          cardProps.cards = props.game.playedCards;
+          cardProps.cardClickCb = props.handleVoteClick;
+          break;
+        default:
+          cardProps.helpText = `Your cards`;
+          cardProps.cardClickCb = props.handleCardClick;
+          cardProps.cards = props.game.cardsOnHand;
+      }
+    }
+    return cardProps as CardsProps;
+  }
+  const state = getCardsState();
+  const isMobile = window.screen.width < 800;
+  if (isMobile) {
+    return (
+      <Box>
+        <Typography color={'white'} variant={'body1'} align={'center'}>
+          {state.helpText}
+        </Typography>
+        <Box sx={{
+          display: 'flex',
+          overflow: 'scroll',
+          marginTop: '2vh',
+          scrollSnapType: 'x mandatory',
+        }}>
+          <CardsList cards={state.cards} blackCard={state.blackCard} cardClickCb={state.cardClickCb}/>
+        </Box>
+      </Box>
+    )
+  } else {
+    return (
+      <Box>
+        <Typography color={'white'} variant={'h5'} align={'center'}>
+          {state.helpText}
+        </Typography>
+        <Box sx={{display: 'flex', justifyContent: 'center', marginTop: '2vh'}}>
+          <CardsList cards={state.cards} blackCard={state.blackCard} cardClickCb={state.cardClickCb}/>
+        </Box>
+      </Box>
+    )
+  }
+}
+
+export default RenderCards
