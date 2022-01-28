33ea6464a617e5feb866530530c1646789b3cae1
Albin Rohde <albin.rohde>
2022-01-28 14:08:57 +0100

    sentry ts config

--- server/src/app.ts
+++ server/src/app.ts
@@ -27,6 +27,19 @@ import {
 } from "./game/events";
 import {SocketServer} from "./lib/socket/Socket";
 
+
+/** Sentry config **/
+// This allows TypeScript to detect our global value
+declare global {
+  namespace NodeJS {
+    interface Global {
+      __rootdir__: string;
+    }
+  }
+}
+global.__rootdir__ = __dirname || process.cwd();
+
+
 dotenv.config({path: '.env'})
 
 export interface ServerOptions {
