276920a9c4d567e00f7d807280af6b7c803051cf
Albin Rohde <albin.rohde>
2022-02-06 22:39:15 +0100

    refactor socket lib to not import global types
    With this refactor we isolate the "socket lib". Removing all it's dependencies of types outside of the lib module.
    
    The goal of this commit is to make the socket lbi more isolated and stand alone.
    
    To achive the same behaviour as before we instead rely on the implementation of the lib sending in the correct arguments.
    
    For example the creater of the SocketServer will have to send in all middlewares

--- server/src/lib/socket/types.ts
+++ server/src/lib/socket/types.ts
@@ -1,4 +1,11 @@
-import {Server} from "socket.io";
-import {SocketWithSession} from "../../types";
+import {Server, Socket} from "socket.io";
 
-export type EventFunction = (io: Server, socket: SocketWithSession, ...args: unknown[]) => Promise<void>
+export type EventFunction = (io: Server, socket: Socket, ...args: unknown[]) => Promise<void>
+export type OnceMiddleware = (socket: Socket, ...args: unknown[]) => void
+export type MiddlewareMetaData = {
+  event: string,
+  args: unknown[],
+  handler?: string,
+}
+export type Middleware = (socket: Socket, meta: MiddlewareMetaData) => unknown
+export type OnErrorHandler = (socket: Socket, error: Error) => unknown
