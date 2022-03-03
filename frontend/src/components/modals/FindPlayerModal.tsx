cd8a93d73c4f6b5b7c250878cb64c886e98caffb
Albin Rohde <albin.rohde@kundo.se>
2022-03-03 22:43:34 +0100

    Display users own created decks

---
+++ frontend/src/components/modals/FindPlayerModal.tsx
@@ -0,0 +1,161 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import {Button, TextField} from "@mui/material";
+import RestClient from "../../clients/RestClient";
+import {useEffect, useState} from "react";
+import {UserData} from "../../clients/ResponseTypes";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableRow from "@mui/material/TableRow";
+import TableBody from "@mui/material/TableBody";
+import TableContainer from "@mui/material/TableContainer";
+import {styled} from "@mui/material/styles";
+import TableCell, {tableCellClasses} from "@mui/material/TableCell";
+import {useDispatch} from "react-redux";
+import {setError} from "../../redux/redux";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: '#2f333d',
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: '#38404f',
+  },
+  '&:nth-of-type(even)': {
+    backgroundColor: '#2f333d',
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+interface Props {
+  open: boolean;
+  deckId: number;
+  setOpen: (open: boolean) => void;
+  addUser: (user: UserData) => void;
+}
+
+export const FindPlayerModal = (props: Props) =>  {
+  const [users, setUsers] = useState<UserData[]>([]);
+  const dispatch = useDispatch();
+  const rest = new RestClient();
+  const isMobile = window.screen.width < 800;
+
+  const searchPlayer = async (e) => {
+    try {
+      if (!e.target.value) {
+        setUsers([]);
+        return;
+      }
+      const players = await rest.makeRequest<UserData[]>({
+        method: 'get',
+        route: 'user',
+        action: '/search',
+        query: {username: e.target.value},
+      });
+      setUsers(players);
+    } catch (err) {
+      if (err.message) {
+        dispatch(setError(err.message));
+      }
+    }
+  }
+
+  const sendInvite = async (user: UserData): Promise<void> => {
+    try {
+      await rest.makeRequest<'ok'>({
+        method: 'post',
+        route: 'deck',
+        action: 'invite',
+        data: { cardDeckId: props.deckId, userId: user.id }
+      })
+      props.addUser(user);
+      props.setOpen(false);
+    } catch (err) {
+      if (err.message) {
+        dispatch(setError(err.message));
+      }
+    }
+  }
+
+  const renderUsers = (users: UserData[]) => {
+    if (users.length < 1) {
+      return;
+    }
+    return (
+        <TableContainer component={Paper} sx={{marginBottom: '4vh'}}>
+          <Table aria-label="customized table">
+            <TableBody>
+              {users.map((user) => (
+                <StyledTableRow key={user.id}>
+                  <StyledTableCell align={'left'} component="th" scope="row">
+                    {user.username}
+                  </StyledTableCell>
+                  <StyledTableCell align={'right'} component="th" scope="row">
+                    <Button color={'success'} variant={'outlined'} onClick={() => sendInvite(user)}>
+                      Invite
+                    </Button>
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )
+  }
+
+  useEffect(() => {
+    if (props.open === false) {
+      setUsers([]);
+    }
+  }, [props.open])
+
+  return (
+    <Modal
+      open={props.open}
+      onClose={() => props.setOpen(false)}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={{display: 'flex', justifyContent: 'center', marginTop: '30vh'}}>
+        <Box sx={{
+          width: isMobile ? '85vw' : '25vw',
+          backgroundColor: '#282c34',
+          borderRadius: '10px',
+          padding: '15px',
+        }}>
+          <Box sx={{display: 'flex', justifyContent: 'center'}}>
+            <Typography color={'#8795ab'} id="modal-modal-title" variant="h4">
+              Search for player
+            </Typography>
+          </Box>
+          <Box sx={{display: 'flex', justifyContent: 'center', marginTop: '2%'}}>
+            <TextField
+              InputLabelProps={{
+                style: { color: '#8795ab', borderRadius: '5px' },
+              }}
+              onChange={searchPlayer}
+              sx={{backgroundColor: '#282c34', input: { color: '#8795ab' }, borderRadius: '10px'}}
+              variant={'outlined'}
+              label="Username"
+            />
+          </Box>
+          <Box sx={{display: 'flex', justifyContent: 'center', marginTop: '2vh', minHeight: '5vh'}}>
+            {renderUsers(users)}
+          </Box>
+        </Box>
+      </Box>
+    </Modal>
+  );
+}
--- frontend/src/screens/loggedIn/ManageDecks.tsx
+++ frontend/src/screens/loggedIn/ManageDecks.tsx
@@ -8,28 +8,26 @@ import React, {useEffect, useState} from "react";
 import {
   AddCircleOutline,
   DeleteForever,
-  EditOutlined,
   InfoOutlined
 } from "@mui/icons-material";
 import RestClient from "../../clients/RestClient";
-import {CardDeckResponse, UserData} from "../../clients/ResponseTypes";
+import {CardDeckResponse, CardDeckUsersResponse, UserData} from "../../clients/ResponseTypes";
 import Divider from "@mui/material/Divider";
 import {AddDeckModal} from "../../components/modals/AddDeckModal";
-import {EditDeckModal} from "../../components/modals/EdiDeckModal";
 import {useDispatch} from "react-redux";
 import {setError} from "../../redux/redux";
+import {DeckCard} from "../../components/DeckCard";
 
 interface Props {
   setScreen: (screen: 'home' | 'create-game' | 'decks') => void,
   user: UserData,
 }
 export const ManageDecks = (props: Props) => {
-  const [myDecks, setMyDecks] = useState<CardDeckResponse[]>([]);
+  const [myDecks, setMyDecks] = useState<CardDeckUsersResponse[]>([]);
   const [libraryDecks, setLibraryDecks] = useState<CardDeckResponse[]>([]);
   const [publicDecks, setPublicDecks] = useState<CardDeckResponse[]>([]);
   const [invites, setInvites] = useState<CardDeckResponse[]>([]);
   const [addModalOpen, setAddModalOpen] = useState<boolean>(false);
-  const [editDeck, setEditDeck] = useState<CardDeckResponse>(null);
   const dispatch = useDispatch();
 
   const rest = new RestClient();
@@ -52,10 +50,6 @@ export const ManageDecks = (props: Props) => {
     }
   }
 
-  const onEditDeck = (deck: CardDeckResponse) => {
-    setEditDeck(deck);
-  }
-
   const deleteDeckFromLibrary = async (deckId: number) => {
     try {
       const deletedDeck = await rest.makeRequest<CardDeckResponse>({
@@ -75,7 +69,7 @@ export const ManageDecks = (props: Props) => {
   }
 
   const fetchMyDecks = async () => {
-    const data = await rest.makeRequest<CardDeckResponse[]>({
+    const data = await rest.makeRequest<CardDeckUsersResponse[]>({
       method: 'get',
       route: 'deck',
       action: 'created',
@@ -112,24 +106,22 @@ export const ManageDecks = (props: Props) => {
 
   const renderMyDecks = () => {
     return myDecks.map((deck) => {
-      return (
-        <Chip
-        onDelete={() => onEditDeck(deck)}
-        color={deck.public ? 'success' : 'primary'}
-        label={deck.name}
-        variant={'outlined'}
-        sx={{marginLeft: '1vh'}}
-        deleteIcon={<Tooltip title={'edit'}><EditOutlined/></Tooltip>}
-        size={'medium'}
-        style={{height: '4vh'}}
-        />
-      )
+      return <DeckCard
+        id={deck.id}
+        title={deck.name}
+        description={deck.description}
+        public={deck.public}
+        addedUsers={deck.users.added}
+        invitedUsers={deck.users.invited}
+        currentUser={props.user}
+      />
     })
   }
 
   const renderLibraryDecks = () => {
-    return libraryDecks.map((deck) => {
-      if (deck.owner == props.user.id) {
+    return libraryDecks
+      .filter((deck) => deck.owner !== props.user.id)
+      .map((deck) => {
         return (
           <Chip
             color={'primary'}
@@ -137,22 +129,11 @@ export const ManageDecks = (props: Props) => {
             variant={'outlined'}
             sx={{marginLeft: '1vh'}}
             size={'medium'}
+            onDelete={() => deleteDeckFromLibrary(deck.id)}
+            deleteIcon={<DeleteForever/>}
             style={{height: '4vh'}}
           />
         )
-      }
-      return (
-        <Chip
-          color={'primary'}
-          label={deck.name}
-          variant={'outlined'}
-          sx={{marginLeft: '1vh'}}
-          size={'medium'}
-          onDelete={() => deleteDeckFromLibrary(deck.id)}
-          deleteIcon={<DeleteForever/>}
-          style={{height: '4vh'}}
-        />
-      )
     })
   }
   const reFetch = async (): Promise<void> => {
@@ -183,28 +164,14 @@ export const ManageDecks = (props: Props) => {
         invites={invites}
         addDeckToLibrary={addDeckToLibrary}
       />
-      <EditDeckModal
-        open={!!editDeck}
-        setOpen={setEditDeck}
-        deck={editDeck}
-        user={props.user}
-        reFetch={reFetch}
-      />
       <Box sx={{display: 'flex', justifyContent: 'center', marginTop: '4vh'}}>
         <Typography color={'#8795ab'} variant={'h4'}>
           Your Decks <Tooltip title={yourDecksToolTip}><Icon><InfoOutlined/></Icon></Tooltip>
         </Typography>
       </Box>
       <Box sx={{display: 'flex', justifyContent: 'center', marginTop: '2vh'}}>
-        <Box sx={{width: '30vw'}}>
+        <Box sx={{width: '60vw', display: 'flex', justifyContent: 'center'}}>
           {renderMyDecks()}
-          <Button
-            disabled
-            variant="outlined"
-            sx={{minHeight: '4vh', marginLeft: '1vh', borderRadius: '15px'}}
-          >
-            <AddCircleOutline/>
-          </Button>
         </Box>
       </Box>
       <Box sx={{display: 'flex', justifyContent: 'center'}}>
