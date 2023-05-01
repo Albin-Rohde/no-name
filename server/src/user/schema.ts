7e4739ea9036ef3fb6adc78b103082a00024754c
Albin Rohde <albin.rohde@kundo.se>
2023-05-01 18:29:32 +0200

    Trim email adresses to remove unwanted whitespace

--- server/src/user/schema.ts
+++ server/src/user/schema.ts
@@ -1,26 +1,26 @@
 import * as yup from 'yup'
 
 export const loginSchema = yup.object({
-  email: yup.string().email('Not a valid email').required(),
+  email: yup.string().trim().email('Not a valid email').required(),
   password: yup.string().required(),
 })
 export interface LoginInput extends yup.Asserts<typeof loginSchema> {}
 
 export const createSchema = yup.object({
-  email: yup.string().email('Not a valid email').required(),
+  email: yup.string().trim().email('Not a valid email').required(),
   password: yup.string().required(),
   username: yup.string().required().max(20),
 })
 export interface CreateInput extends yup.Asserts<typeof createSchema> {}
 
 export const emailSchema = yup.object({
-  email: yup.string().required(),
+  email: yup.string().trim().required(),
 })
 export interface EmailInput extends yup.Asserts<typeof emailSchema> {}
 
 export const updateSchema = yup.object({
   id: yup.number().required(),
-  email: yup.string().email('Not a valid email'),
+  email: yup.string().trim().email('Not a valid email'),
   password: yup.string(),
   username: yup.string(),
 }).test(
